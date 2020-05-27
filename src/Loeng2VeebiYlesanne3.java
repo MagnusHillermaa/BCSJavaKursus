c777df0bb618780e8fc7e210ec3310f7aa36ae65
hillermaa <magnus.hillermaa@outlook.com>
2020-05-27 16:55:03 +0300

    keeruka graafi lahenduse esimesed mõtted

---
+++ src/Loeng2VeebiYlesanne3.java
@@ -0,0 +1,8 @@
+public class Loeng2VeebiYlesanne3 {
+    //graa
+    // node struktuur
+        //gatewayConnections - number of gateways connected to node
+        //shortest way player int
+        //priority shortest way to player -gatewayConnections
+    //number of connected links
+}
